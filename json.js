07a594399c292d386de9091cb0af82994c786c2f
tushar00jain <tushar00jain@ymail.com>
2015-11-04 11:28:18 +0800

    finished

--- json.js
+++ json.js
@@ -1,11 +1,38 @@
 var fs = require('fs')
 var http = require('http')
-var map = require('through2-map')
+var url = require('url')
 
 var server = http.createServer(function(request, response) {
 	if(request.method != 'GET') {
 		console.error("error")
 	}
+
+	json = url.parse(request.url, true)
+	var date = new Date(json.query.iso)
+
+	if(json.pathname === "/api/parsetime") {
+		response.writeHead(200, {"Content-Type" : "application/json"})
+		response.end(JSON.stringify(
+			{
+			hour: date.getHours(),
+			minute: date.getMinutes(),
+			second: date.getSeconds()
+		}
+		))
+
+	} else if(json.pathname === "/api/unixtime") {
+		response.writeHead(200, {"Content-Type" : "application/json"})
+		date = new Date
+		response.end(JSON.stringify(
+			{
+			"unixtime": date.getTime()
+		}
+		))
+	} else {
+		response.writeHead(404)
+		response.write()
+	}
+
 })
 
 server.listen(Number(process.argv[2]))
